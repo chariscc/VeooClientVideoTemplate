a82f9d778ffd2961f007a37ca6dc4e596c7e904f
chariscc <charis@veoo.com>
2016-06-13 13:19:54 +0300

    removed comments

--- wp-content/themes/twentythirteen/page.php
+++ wp-content/themes/twentythirteen/page.php
@@ -1,16 +1,3 @@
-
-/**
- * The template for displaying all pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages and that other
- * 'pages' on your WordPress site will use a different template.
- *
- * @package WordPress
- * @subpackage Twenty_Thirteen
- * @since Twenty Thirteen 1.0
- */
-
 	<div id="primary" class="content-area">
 		<div id="content" class="site-content" role="main">
 
