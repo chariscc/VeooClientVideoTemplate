bc7de1b59311a60f32b21a8fbcd6c35fc83b752e
chariscc <charis@veoo.com>
2016-06-16 11:16:26 +0300

    added amazon user for plugin

--- wp-config.php
+++ wp-config.php
@@ -46,6 +46,9 @@ define('DB_COLLATE', '');
  * @since 2.6.0
  */
 
+define( 'AWS_ACCESS_KEY_ID', 'AKIAIEWKL6UG7LTNU6VQ' );
+define( 'AWS_SECRET_ACCESS_KEY', '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' );
+
 /**#@-*/
 
 /**
